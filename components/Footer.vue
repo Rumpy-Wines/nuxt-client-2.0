62b5cab22c7cb937cb29c3baaea5723ca14ea80b
Oyinbo David <dev.davexoyinbo@gmail.com>
2020-11-05 01:16:40 +0100

    Footer

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="footer">
+		<div class="content">
+			<div class="brand">
+				<div class="logo-icon"></div>
+				<span>rumpy &nbsp;</span>
+				<span class="accent">wines</span>
+			</div>
+			<div class="nav-links-container">
+				<div class="nav-links">
+				<div class="nav-link active">
+				<i class="icon fas fa-store"></i> Product
+				</div>
+				<div class="nav-link">
+				<i class="icon fas fa-hands-helping"></i> Help Center
+				</div>
+				<div class="nav-link">
+				<i class="icon fas fa-user-alt"></i> Account
+				</div>
+				<div class="nav-link">
+				<i class="icon fas fa-sign-in-alt"></i> Login
+				</div>
+				<div class="nav-link">
+				<i class="icon fas fa-cart-arrow-down"></i> Cart
+				</div>		
+			</div>
+			</div>
+		</div>
+		<div class="footer-footer">
+			<small>&copy; 2020 Rumpy Wines</small>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@import '~assets/styles/components/footer';
+</style>
